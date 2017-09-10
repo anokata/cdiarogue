2557468a10415a77809e32b139e9a22a9029b9e3
anokata <anokata8@mail.ru>
2017-09-10 22:09:37 +0300

    (CR) (Made) map edit script, and map replace data

---
+++ crouge0/tools/mapreplace.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "tile_map.h"
+
+int main(int argc, char* argv[]) {
+    UNUSED(argc);
+    char *map_name = argv[1];
+    if (map_name) {
+    
+    } else {
+        map_name = "./maps/map_1_1";
+    }
+
+    // Read from stdin
+    char *map_data = NULL;
+    size_t len = 0;
+    getline(&map_data, &len, stdin);
+
+    TileMap l = load_tile_map(map_name);
+
+    copy_map2tiles(l, map_data, strlen(map_data), 0);
+
+    print_tile_map(l);
+    free_tile_map(l);
+    free(map_data);
+    map_data = NULL;
+}
