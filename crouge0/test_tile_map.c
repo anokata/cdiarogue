0446b87acacf53933b0be030f3c718680cbdbff2
quartenium <quartenium@gmail.com>
2017-10-03 16:37:38 +0300

    (CR) (Add) objs of location, clean up tile map test

--- crouge0/test_tile_map.c
+++ crouge0/test_tile_map.c
@@ -1,15 +1,25 @@
 #include "tile_map.h"
 
-int main() {
+void test_tmap_create_load() {
     TileMap m = make_tile_map(2, 3);
     print_tile_map(m);
-    /* TileMap l = load_tile_map("maps/map_1_1"); */
-    TileMap l = load_tile_map("maps/map_0_1");
+    TileMap l = load_tile_map("maps/map_1_1");
     print_tile_map(l);
     free_tile_map(l);
     free_tile_map(m);
 
-    TileMap g = load_global_tmap();
+    TileMap g = load_global_tmap("maps/loc1");
     print_tile_map(g);
     free_tile_map(g);
+}
+
+void test_load_loc() {
+    TileMap g = load_global_tmap("maps/loc1");
+    /* print_tile_map(l); */
+    free_tile_map(g);
+}
+
+int main() {
+    /* test_tmap_create_load(); */
+    test_load_loc();
 }
