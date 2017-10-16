426c01c15dbce61622712465c0ad5a1016788c4f
anokata <anokata8@mail.ru>
2017-10-16 21:27:33 +0300

    Move: study stuff to other repo

--- cexer2/glog.c
+++
@@ -1,9 +0,0 @@
-#include <glib.h>
-#include "debug.h"
-#include "debug.h"
-
-int main() {
-    printf("Glib log test\n");
-    g_message("g_message\n"); // to stdio
-    debug_file_log("msg #1\n");
-}
--- net/main.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char* argv[]) {
-    printf("2017 (c)\n");
-}
