229782aabe5c8400bac2aa66a4602d65e4c63ff4
quartenium <quartenium@gmail.com>
2018-08-14 12:52:21 +0300

    [init] kernel module hello world

---
+++ kernelmod/hw.c
@@ -0,0 +1,22 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+void sayhi() {
+    printk(KERN_INFO"Hi, from mod\n");
+}
+
+EXPORT_SYMBOL(sayhi);
+
+static int __init hi_init(void) {
+    sayhi();
+    return 0;
+}
+
+static void __exit hi_exit(void) {
+    printk(KERN_INFO"goodby\n");
+}
+
+module_init(hi_init);
+module_exit(hi_exit);
+MODULE_LICENSE("GPL");
